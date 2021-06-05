553dc5e7dd98a2490a4340d91236f8a170d9f257
Ronier Lima <ronier.lim@gmail.com>
2021-06-05 14:12:25 -0300

    Finalizando questão @15 - pertence em js

--- atividades/atv-03/javascript/cat-04.js
+++ atividades/atv-03/javascript/cat-04.js
@@ -9,8 +9,61 @@ result;
 // @007 somaImpares
 
 const somaImpares = (xs) =>
-  xs.filter((x) => x % 2 != 0).reduce((soma, x) => soma + x);
+  xs.filter((x) => x % 2 != 0).reduce((soma = 0, x) => soma + x);
 
 result = somaImpares([1, 2, 3]);
 result;
 
+// @009 sublist
+
+const sublist = (inicio) => (fim) => (xs) =>
+  xs.filter(
+    (elem, index) =>
+      index >= (inicio < 0 ? inicio + xs.length : inicio) &&
+      index < (fim < 0 ? fim + xs.length : fim) &&
+      elem
+  );
+
+result = sublist(-10)(-1)([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+result;
+
+// @012 fatorial
+
+const fatorial = (n) => (n < 2 ? 1 : n * fatorial(n - 1));
+
+result = fatorial(6);
+result;
+
+// @014 elemento
+
+const elemento = (index) => (xs) => {
+  index = index < 0 ? index + xs.length : index;
+
+  if (index == 0) return xs[0];
+
+  return elemento(index - 1)(xs.slice(1));
+};
+
+result = elemento(1)([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+
+result;
+
+// @015 pertence
+
+// Opção 1
+// const pertence = (n) => (xs) => xs.includes(n);
+
+// Opção 2
+// const pertence = (n) => (xs) => !(!xs.filter(elem => elem == n).length)
+
+// Opção 3
+const pertence = (n) => (xs) => {
+  if (xs.length === 0) return false;
+  if (xs[0] === n) return true;
+
+  return pertence(n)(xs.slice(1));
+};
+
+result = pertence(1)([0, 1, 1, 1, 4, 5, 6, 7, 8, 9, 10]);
+
+result;
